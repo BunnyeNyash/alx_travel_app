7670faf85c137ae73f77a1c59297ef4fadfe00f7
BunnyeNyash <otachibosibori@gmail.com>
2025-05-18 21:20:09 +0300

    urls

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -20,6 +20,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
+
+# Setting up Swagger with project metadata
 schema_view = get_schema_view(
     openapi.Info(
         title="ALX Travel App API",
@@ -34,8 +36,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('listings.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls),        # Routes to Django admin
+    path('api/', include('listings.urls')), # Routes to listings/urls.py
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),        # Routes to Swagger UI
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),                 # Routes to ReDoc UI
 ]
